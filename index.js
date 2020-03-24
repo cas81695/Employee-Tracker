147267a02cc18c7b327f2319cd785b979e147101
Christian Sanchez <cas81695@gmail.com>
2020-03-24 15:39:46 -0400

    added schema

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const connection = require("connection");
 const mysql = require("mysql");
 
 
+const connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "C@s81695",
+	database: "employee_trackerDB",
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+});
+
 
 const askQuestions = function() {
     inquirer
@@ -191,8 +203,7 @@ function addEmployee() {
         askQuestions();
       });
   }
-  
-  // allows user to add a new role/title
+
   function addRole() {
     inquirer
       .prompt([
---
+++ schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department
+(
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL (9,2),
+  department_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee
+(
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
