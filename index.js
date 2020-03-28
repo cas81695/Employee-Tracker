fc7ba9deda8ff6f32acf555657e267941c46a13e
Christian Sanchez <cas81695@gmail.com>
2020-03-27 21:48:44 -0400

    final edits

--- index.js
+++ index.js
@@ -107,14 +107,14 @@ function viewEmployee() {
 function viewEmployeeByDepartment() {
   console.log("Viewing employees by department\n");
 
-  var query =
-    `SELECT d.id, d.name, r.salary AS budget
+  var query = `SELECT d.id, d.name, SUM(r.salary) AS budget
   FROM employee e
   LEFT JOIN role r
 	ON e.role_id = r.id
   LEFT JOIN department d
   ON d.id = r.department_id
-  GROUP BY d.id, d.name`
+  GROUP BY d.id, d.name`;
+
 
   connection.query(query, function (err, res) {
     if (err) throw err;
@@ -367,14 +367,13 @@ function promptEmployeeRole(employeeChoices, roleChoices) {
 
 function addRole() {
 
-  var query =
-    `SELECT d.id, d.name, r.salary AS budget
-    FROM employee e
-    JOIN role r
-    ON e.role_id = r.id
-    JOIN department d
-    ON d.id = r.department_id
-    GROUP BY d.id, d.name`
+  var query = `SELECT d.id, d.name, SUM(r.salary) AS budget
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  GROUP BY d.id, d.name`;
 
   connection.query(query, function (err, res) {
     if (err) throw err;
